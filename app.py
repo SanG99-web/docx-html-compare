ea16093791d52ccc3ce782de3752eb2e5105e02e
SanG99-web <sanjay.marathe@g99.plus>
2025-05-23 17:41:15 +0530

    app.py

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def download_result(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
