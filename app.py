9b4603230d18199055b83044d950129594b51030
SanG99-web <sanjay.marathe@g99.plus>
2025-05-23 18:21:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,4 @@ def download_result(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', debug=True)
